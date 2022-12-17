f91136062aaf0acdfe7c50401fa215e34e1e948d
Anakole <104356239+Anakole@users.noreply.github.com>
2022-12-17 13:34:19 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,60 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Box } from './App.styled';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Notification } from './Notification/Notification';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onElementClick = e => {
+    switch (e.target.value) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  onElementClick = element => {
-    const keyState = Object.keys(this.state);
+  const totalFeedbacks = good + neutral + bad;
+  const feedbackPercentage = Math.floor((good / totalFeedbacks) * 100);
+  const options = Object.keys({ good, neutral, bad });
 
-    keyState.forEach(key => {
-      if (element.target.value === key) {
-        this.setState(prevState => ({
-          [key]: prevState[key] + 1,
-        }));
-      }
-    });
-  };
-
-  totalFeedbacks() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  }
-
-  feedbackPercentage() {
-    const { good } = this.state;
-    return Math.floor((good / this.totalFeedbacks()) * 100);
-  }
-  render() {
-    const { good, neutral, bad } = this.state;
-    const keyState = Object.keys(this.state);
-    return (
-      <Box>
-        <Section
-          title="Please leave feedback"
-          children={
-            <FeedbackOptions
-              options={keyState}
-              onLeaveFeedback={this.onElementClick}
+  return (
+    <Box>
+      <Section
+        title="Please leave feedback"
+        children={
+          <FeedbackOptions options={options} onLeaveFeedback={onElementClick} />
+        }
+      />
+      <Section
+        title="Statistics"
+        children={
+          totalFeedbacks > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedbacks}
+              positiveFeedback={feedbackPercentage}
             />
-          }
-        />
-        <Section
-          title="Statistics"
-          children={
-            this.totalFeedbacks() > 0 ? (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={this.totalFeedbacks()}
-                positiveFeedback={this.feedbackPercentage()}
-              />
-            ) : (
-              <Notification message={'There is no feedback'} />
-            )
-          }
-        />
-      </Box>
-    );
-  }
-}
+          ) : (
+            <Notification message={'There is no feedback'} />
+          )
+        }
+      />
+    </Box>
+  );
+};
